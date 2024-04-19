42d7d3937dcfc9e037fe48e20a14593039bb7553
StandingR <106955277+StandingR@users.noreply.github.com>
2024-04-19 13:03:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 118.84 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,15 +1,19 @@
+
 def solution(s):
     stack = []
-    for i in range(len(s)):
+    for output in range(len(s)):
         if not stack:
-            stack.append(s[i])
+            stack.append(s[output])
         else:
-            if s[i] == stack[-1]:
-                stack.pop
+            if s[output] == stack[-1]:
+                stack.pop()
             else:
-                stack.append(s[i])
-    if stack : return ture
-    else :  return false
+                stack.append(s[output])
+    if stack: 
+        return 0
+    else:  
+        return 1
+    
     
 
 
@@ -23,12 +27,4 @@ def solution(s):
 
 
 
-def solution(s):
-  stack = [ ]  # 스택 초기화
-  for c in s:
-    if stack and stack[-1] == c:  # ➊ 스택이 비어 있지 않고, 
-       # 현재 문자와 스택의 맨 위 문자가 같으면
-      stack.pop( )   # ➋ 스택의 맨 위 문자 제거
-    else:
-      stack.append(c)    # ➌ 스택에 현재 문자 추가
-  return int(not stack)  # ➍ 스택이 비어 있으면 1, 그렇지 않으면 0 반환 
+
